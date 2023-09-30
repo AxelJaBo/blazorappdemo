df81016978ba030cce0b31c06eda90a7d2a7bb77
AxelJaBo <axel.910921@gmail.com>
2023-09-30 08:44:03 -0700

    Creando formulario para agregar registros

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,10 +3,10 @@ namespace blazorappdemo;
 public class Product
 {
     public int Id { get; set; }
-    public string title { get; set; }
-    public decimal? price { get; set; }
+    public string Title { get; set; }
+    public decimal? Price { get; set; }
     public string Description { get; set; }
     public int CategoryId { get; set; }
     public string[] Images { get; set; }
-    public string? image { get; set; }
+    public string? Image { get; set; }
 }
