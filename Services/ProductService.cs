7dac25ab9f49825f2a8c7a80eba7e3cdaed7740c
AxelJaBo <axel.910921@gmail.com>
2023-09-30 09:05:38 -0700

    Integrando formulario con servicios

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,50 +1,58 @@
 using System.Net.Http.Json;
 using System.Text.Json;
+
 namespace blazorappdemo;
 
 public class ProductService : IProductService
 {
     private readonly HttpClient client;
-    private readonly JsonSerializerOptions options;
-    public ProductService(HttpClient httpClient)
+
+    private readonly JsonSerializerOptions options; 
+
+    public ProductService( HttpClient httpClient)
     {
         client = httpClient;
-        options=new JsonSerializerOptions{PropertyNameCaseInsensitive =true};
+        options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
     }
 
-    public async Task<List<Product>?> Get()
+        public async Task<List<Product>?> Get()
+        {
+            var response = await client.GetAsync("products");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+
+            return JsonSerializer.Deserialize<List<Product>>(content, options);
+        }
+
+        public async Task Add(Product product)
+        {
+            var response = await client.PostAsync("products", JsonContent.Create(product));
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+        }
+
+        public async Task Delete(int productId)
+        {
+            var response = await client.DeleteAsync($"products/{productId}");
+            var content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException(content);
+            }
+        }
+}
+
+ public interface IProductService
     {
-        var response = await client.GetAsync("/products");
-        var content = await response.Content.ReadAsStringAsync();
-        if(!response.IsSuccessStatusCode)
-        {
-            throw new ApplicationException(content);
-        }
-        return JsonSerializer.Deserialize<List<Product>>(content, options);
-    }
+        Task<List<Product>?> Get();
 
-    public async Task Add(Product product){
-        var response=await client.PostAsync("/products", JsonContent.Create(product));
-        var content= await response.Content.ReadAsStringAsync();
-        if (!response.IsSuccessStatusCode)
-        {
-            throw new ApplicationException(content);
-        }
-    }
+        Task Add(Product product);
 
-    public async Task Delete(int productId){
-        var response=await client.DeleteAsync($"products/{productId}");
-        var content= await response.Content.ReadAsStringAsync();
-        if (!response.IsSuccessStatusCode)
-        {
-            throw new ApplicationException(content);
-        } 
+        Task Delete(int productId);
     }
-}
-
-public interface IProductService
-{
-    Task<List<Product>?> Get();
-    Task Add(Product product);
-    Task Delete(int productId);
-}
